08005cafef51cac93d51a08ac0110769149afe63
Slomo <slomochacko242@gmail.com>
2024-04-18 16:20:56 -0400

    updated dag

--- dags/01_load_dim.py
+++ dags/01_load_dim.py
@@ -3,6 +3,7 @@ from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.amazon.aws.operators.redshift_data import RedshiftDataOperator
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 
@@ -25,10 +26,12 @@ with DAG(
 ) as dag:
 
     # Create schema if it doesn't exist
-    create_schema = PostgresOperator(
+    create_schema = RedshiftDataOperator(
         task_id='create_schema',
-        postgres_conn_id='redshift',
+        database='dev',
         sql="CREATE SCHEMA IF NOT EXISTS food_delivery_db;",
+        db_user='awsuser',
+        aws_conn_id='redshift'
     )
 
     # Drop tables if they exist
